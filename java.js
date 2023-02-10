ade5f249a5efc085ab20a1b48cfac5a6eab10774
aleshn1 <122236649+aleshn1@users.noreply.github.com>
2023-02-09 22:56:15 -0300

    Update java.js

--- java.js
+++ java.js
@@ -4,7 +4,9 @@ let chave = "d3f03dfa5d2a44756ee0afc07e0c0ea8"
 function colocarNaTela(dados){
     console.log(dados)
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-        
+    document.querySelector(".temp").innerHTML = Math.floor (dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".nuvem").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
 }
 async function buscarCidade(cidade){
     let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
